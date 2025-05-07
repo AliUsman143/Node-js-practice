c2ba03942e455119c254d191f09afc86cdd5336f
Your Name <aliusmanijaz143@gmail.com>
2025-05-07 12:46:06 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const http = require("http");
+http
+  .createServer((req, resp) => {
+    resp.write("<h1> This is node js server </h1>");
+    resp.end();
+  })
+  .listen(4500);
+
+// ---------------------------------------
+// const fs=require("fs");
+// fs.writeFileSync("hello.txt","hello world"); // hello world
+
+
+// ---------------------------------------
+// const app=require("./app.js");
+
+// // console.log(app); // 20
+
+// // var a=20;
+// // var b=20;
+// // var c=20;
+// // console.log(a+b+c); // 60
+// let arry=[1,2,3,4,5,6,7,8,9,10];
+// arry.filter((item)=>{
+// console.log(item); // 1 2 3 4 5 6 7 8 9 10
+// })
